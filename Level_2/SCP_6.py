b799d54c9f357a6ad8f0f8ce737e398230112629
Andrey Melnikov <k253vo@mail.ru>
2020-04-09 15:14:25 +0400

    lev2-6.1-to-check

--- Level_2/SCP_6.py
+++ Level_2/SCP_6.py
@@ -1,8 +1,20 @@
 # ЗАДАНИЕ 13
-"""В данном задании, в качестве возможного решения, я реализовал промежуточный класс ElementaryStruct, также входящий
- в общую систему классов (General,Any...NoneType), в котором была реализована операция сложения над типами (структурами)
-  list, которые также будут являться базой для нашего класса Vector."""
-
+"""
+В данном задании, в качестве возможного решения, я реализовал класс ElementaryStruct, также входящий
+ в общую систему классов (General,Any...NoneType). В классе ElementaryStruct была реализована операция сложения --
+ def summation() для структур с полем 'a'. Класс TypeT является наследником класса ElementaryStruct. Класс Vector, также
+ является  наследником класса ElementaryStruct. Т.о. мы можем предположить, что метод сложения (summation),
+ который определен в классе ElementaryStruct, будет справедлив и для Vector[TypeT]. Vector, имеет свой переопределенный
+ метод сложения (summаtion). Значит, Vector.summation() вызовет у элементов вектора тот же метод, но уже принадлежащий
+ соответствующему собственному классу (или родительскому). Получается своего рода рекурсивное обращение. Каждый метод
+ summation будет вызван у каждого типа свой.
+ Не уверен, что правильно реализовал данное задание, именно в случае для сложения произвольных типов <T>. В моем случае,
+ скорее, все-же получился конкретный тип (класс TypeT, с унаследованным полем а, для арифметических операций).
+ Также несовсем понятен случай, если мы переопределим конструктор для класса TypeT, и уже не сможем гарантированно
+ обращаться с данным типом, как с наследником типа ElementaryStruct. (поля а для операций сложения может и не быть,
+ в случае переопределения конструктора)
+ Тесты для Vector[Vector[Vector[T]]] в самом конце.
+ """
 from abc import ABC, abstractmethod
 
 class General(ABC):
@@ -186,16 +198,12 @@ class Any(General):
         self.__equal_status = self.CONST_EQUAL_ERR
 
     def type_of(self):
-        self_object = Any(1, 1)
         self.__type_of_status = self.CONST_TYPE_OF_OK
-        return type(self_object)
-
+        return type(self)
 
     def is_type(self, object_type):
         self.__is_type_status = self.CONST_IS_TYPE_OK
-        if object_type is not self.type_of():
-            return False
-        return True
+        return isinstance(object_type, self.type_of())
 
     def assignment_attempt(self, target, source):
         self.__assignment_status = self.CONST_ASSIGNMENT_OK
@@ -230,14 +238,14 @@ class Any(General):
         return self.__print_status
 
 class ElementaryStruct(Any):
-    # описываем промежуточный класс, который имеет возможность производить сложение структур типа
-    # list (наш будущий Vector).
+    # описываем промежуточный класс, который имеет возможность производить сложение структур с полем а.
+
     CONST_SUMMATION_NIL = 0
     CONST_SUMMATION_OK = 1
     CONST_SUMMATION_ERR = 2
 
-    def __init__(self):
-        self.struct = []
+    def __init__(self, a):
+        self.a = a
         self.__summation_status = self.CONST_SUMMATION_NIL
         self.__is_type_status = self.CONST_IS_TYPE_NIL
         self.__type_of_status = self.CONST_TYPE_OF_NIL
@@ -245,53 +253,163 @@ class ElementaryStruct(Any):
     def type_of(self):
         # переопределяем метод для определения типа нашей структуры.
         self.__type_of_status = self.CONST_TYPE_OF_OK
-        return type(self.struct)
-
-    def is_type(self, object_type):
-        # переопределяем метод для сравнения типов.
-        self.__is_type_status = self.CONST_IS_TYPE_OK
-        if object_type is not self.type_of():
-            return False
-        return True
+        return type(self)
 
     def summation(self, other_struct):
-        # метод сложения элементов двух структур. Если длины равны, то складываем. Иначе взвращаем Void типа NoneType.
+        # метод сложения полей двух объектов типа ElementaryStruct.
         if self.is_type(other_struct):
             self.__summation_status = self.CONST_SUMMATION_OK
-            if len(self.struct) == len(other_struct):
-                count = []
-                for i in range(len(self.struct)):
-                    count.append(self.struct[i] + other_struct[i])
-                    return count
-            else:
-                void = NoneType()
-                return void
+            return self.a + other_struct.a
         self.__summation_status = self.CONST_SUMMATION_ERR
 
     def get_summation_status(self):
         return self.__summation_status
 
+
 class TypeT(ElementaryStruct, Any):
+    # пользовательский класс, наследник класса ElementaryStruct.
     pass
 
-class Vector(ElementaryStruct, Any):
-    # класс Vector, при помощи множественного наследования, теперь обладает функционалом сложения структур типа List.
-    def __init__(self, n):
-        # создаем структуру Vector длиной n - элементов типа T (TypeT).
-        self.T = TypeT()
-        self.vector = [self.T] * n
 
+class Vector(ElementaryStruct, Any):
+    # класс Vector.
+    CONST_SIZE_OF_NIL = 0
+    CONST_SIZE_OF_OK = 1
+    CONST_SIZE_OF_ERR = 2
+    CONST_GET_ITEM_NIL = 0
+    CONST_GET_ITEM_OK = 1
+    CONST_GET_ITEM_ERR = 2
+    CONST_ADD_IN_NIL = 0
+    CONST_ADD_IN_OK = 1
+
+    def __init__(self):
+        # создаем структуру Vector.
+        self.vector = []
+        self.__size_of_status = self.CONST_SIZE_OF_NIL
+        self.__get_item_status = self.CONST_GET_ITEM_NIL
+        self.__summation_status = self.CONST_SUMMATION_NIL
+        self.__add_in_status = self.CONST_ADD_IN_NIL
+
+    def add_in(self, item):
+        self.__add_in_status = self.CONST_ADD_IN_OK
+        self.vector.append(item)
+
+    def type_of(self):
+        # переопределяем метод для определения типа нашей структуры.
+        self.__type_of_status = self.CONST_TYPE_OF_OK
+        return type(self)
+
+    def summation(self, other_struct):
+        if isinstance(other_struct, self.type_of()) and self.size_of() != 0:
+            # если обе структуры одинакового типа, то мы можем сравнить их длину.
+            self.__summation_status = self.CONST_SUMMATION_OK
+            if self.size_of() == other_struct.size_of():
+                # если длина одинакова, то производим операцию сложения элементов (зная, что тип элементов - наследник
+                # тогоже родительского класса, что и наш класс Vector).
+                result = Vector()
+                for i in range(self.size_of()):
+                    a = self.vector[i]
+                    b = other_struct.vector[i]
+                    summ = a.summation(b)
+                    if a.get_summation_status() != 2:
+                        result.add_in(summ)
+                return result
+            else:
+                void = NoneType()
+                return void
+        self.__summation_status = self.CONST_SUMMATION_ERR
+
+    def size_of(self):
+        if len(self.vector) != 0:
+            self.__size_of_status = self.CONST_SIZE_OF_OK
+            return len(self.vector)
+        return 0
+
+    def get_item(self, index_item):
+        if self.type_of() != 0 and index_item < self.size_of():
+            self.__get_item_status = self.CONST_GET_ITEM_OK
+            return self.vector[index_item]
+        self.__get_item_status = self.CONST_GET_ITEM_ERR
+
+    def print_items(self):
+        for i in range(self.size_of()):
+            print(self.vector[i], end=' ')
+        print()
+
+    def get_summation_status(self):
+        return self.__summation_status
+
+    def get_for_get_item_status(self):
+        return self.__get_item_status
+
+    def get_add_in_status(self):
+        return self.__add_in_status
 
 class NoneType(Vector, TypeT, ElementaryStruct, Any):
     # замыкающий класс
-    pass
+    def __init__(self):
+        pass
 
-"""Мне кажется, что данный способ внесения промежуточного класса для получения новых функциональных возможностей, в 
- зыке Python, можно реализовать при помощи миксинов. По сути миксин, это ведь внесение некой "примеси" в уже действующую 
- иерархию классов.
- В конструкции типа: Vector[Vector[TypeT]], соблюдается условие того, что Vector и TypeT наследники общего класса  
- ElementaryStruct. Если в ElementaryStruct реализовано сложение типов, то оно также будет справедливо и для 
- Vector[Vector[Vector[TypeT]]]. """
+from unittest import TestCase
 
+class TestForVector(TestCase):
+    # создаем объекты типа T
+    T1 = TypeT(1)
+    T2 = TypeT(2)
+    T3 = TypeT(3)
+    # создаем вектора
+    v1 = Vector()
+    v2 = Vector()
+    # и добавляем в них элементы типа Т (Vector[TypeT])
+    v1.add_in(T1)
+    v1.add_in(T2)
+    v2.add_in(T1)
+    v2.add_in(T3)
+    # вектора разной длины
+    va1 = Vector()
+    vb2 = Vector()
+    va1.add_in(T1)
+    va1.add_in(T2)
+    va1.add_in(T3)
+    vb2.add_in(T1)
+    # создаем вектора для хранения векторов (Vector[Vector])
+    vv1 = Vector()
+    vv2 = Vector()
+    vvv1 = Vector()
+    vvv2 = Vector()
+    # добавляем в них вектора с элементами типа Т (Vector[Vector[TypeT]])
+    vv1.add_in(v1)
+    vv2.add_in(v2)
+    vvv1.add_in(vv1)
+    vvv2.add_in(vv2)
 
+    def test_SizeVector(self):
+        self.assertEqual(self.v1.size_of(), 2, 'incorrect size vector v1')
+        self.assertEqual(self.v2.size_of(), 2, 'incorrect size vector v2')
+        self.assertEqual(self.vv1.size_of(), 1, 'incorrect size vector vv1')
+        self.assertEqual(self.vv2.size_of(), 1, 'incorrect size vector vv2')
+        self.assertEqual(self.va1.size_of(), 3, 'incorrect size vector va1')
+        self.assertEqual(self.vb2.size_of(), 1, 'incorrect size vector vb2')
+    def test_SummVectors(self):
+        summ = self.v1.summation(self.v2)
+        self.assertEqual(self.v1.get_summation_status(), 1, 'incorrect summation v1 and v2')
+        self.assertEqual(summ.type_of(), self.v1.type_of(), 'incorrect return type after summation')
+        self.assertEqual(summ.size_of(), 2, 'incorrect size of result')
+        summ1 = self.vv1.summation(self.vv2)
+        self.assertEqual(self.vv1.get_summation_status(), 1, 'incorrect summation vv1 and vv2')
+        self.assertEqual(summ1.type_of(), self.vv1.type_of(), 'incorrect return type after summation')
+        self.assertEqual(summ1.size_of(), 1, 'incorrect size of result')
+        summ2 = self.vvv1.summation(self.vvv2)
+        self.assertEqual(self.vvv1.get_summation_status(), 1, 'incorrect summation vvv1 and vvv2')
+        self.assertEqual(summ2.type_of(), self.vvv1.type_of(), 'incorrect return type after summation')
+        self.assertEqual(summ2.size_of(), 1, 'incorrect size of result')
+        # суммирование векторов различной длины
+        res = self.va1.summation(self.vb2)
+        self.assertEqual(self.va1.get_summation_status(), 1, 'incorrect summation status')
+        self.assertEqual(type(res), type(NoneType()), 'incorrect return type after summation different vectors')
+"""
+t = TestForVector()
+print(t.test_SizeVector())
+print(t.test_SummVectors())
+"""
 
